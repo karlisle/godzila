87bef8c6b0d278694c4821c09d7719998e1add7f
karlisle <inoue.sora@gmail.com>
2016-01-25 18:32:48 -0600

    tracking

---
+++ OpencvPython/cv_io.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#######################################
+# Author: 	Kharl                     #
+# Licence: 	Creative commons          #
+# WebPage: 	karlislee.com             #
+#                                     #
+#######################################
+
+
+from collections import deque
+import numpy as np 
+import argparse
+import imutils
+import cv2
